ec4ea59ec30e769122698ddf01a4cf931e7a0432
jaredrosenlund <jaredrosenlund@JAREDROSENL79AF>
2024-01-17 20:09:31 -0700

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2_DiceRoller
+{
+    internal class DiceRoller
+    {
+        public int[] Play(int iNumRolls)
+        {
+            Random random = new Random();
+
+            // Initialize vars
+            int[] results = [];
+            int two = 0;
+            int three = 0;
+            int four = 0;
+            int five = 0;
+            int six = 0;
+            int seven = 0;
+            int eight = 0;
+            int nine = 0;
+            int ten = 0;
+            int eleven = 0;
+            int twelve = 0;
+
+            int die1 = 0;
+            int die2 = 0;
+
+            // Roll the dice
+            for (int iRollCount = 0; iRollCount < iNumRolls; iRollCount++)
+            {
+                die1 = random.Next(1, 7);
+                die2 = random.Next(1, 7);
+
+                // Iterate the right variable according to the result
+                if (die1 + die2 == 2)
+                { two++; }
+                else if (die1 + die2 == 3)
+                { three++; }
+                else if (die1 + die2 == 4)
+                { four++; }
+                else if (die1 + die2 == 5)
+                { five++; }
+                else if (die1 + die2 == 6)
+                { six++; }
+                else if (die1 + die2 == 7)
+                { seven++; }
+                else if (die1 + die2 == 8)
+                { eight++; }
+                else if (die1 + die2 == 9)
+                { nine++; }
+                else if (die1 + die2 == 10)
+                { ten++; }
+                else if (die1 + die2 == 11)
+                { eleven++; }
+                else if (die1 + die2 == 12)
+                { twelve++; }
+                else { System.Console.WriteLine("Something's wrong :("); }
+            }
+
+            // Compile results
+            results = [two, three, four, five, six, seven, eight, nine, ten, eleven, twelve];
+
+            return results;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using Mission2_DiceRoller;
+using System.Xml;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Function to add asterisks
+        string histAsterisks(int numRolls, int totRolls)
+        {
+            string asterisks = "";
+
+            double doubleRolls = (double)numRolls;
+            double doubleTot = (double)totRolls;
+
+            double percent = (doubleRolls / doubleTot) * 100;
+
+            for (int i = 0; i < percent; i++)
+            {
+                asterisks = asterisks + "*";
+            }
+
+            return asterisks;
+        }
+
+        // Create reference to DiceRoller class
+        DiceRoller roll = new DiceRoller();
+
+        // Initialize dice roller vars
+        int iNumRolls = 0;
+        int[] aResults = [];
+
+        // Get user input
+        System.Console.WriteLine("Welcome to the Dice Rolling Simulator!");
+        System.Console.WriteLine("How many times would you like to roll the dice?");
+        iNumRolls = int.Parse(Console.ReadLine());
+
+        // Give desired rolls to the play method and collect the results
+        aResults = roll.Play(iNumRolls);
+
+        // Print results
+        System.Console.WriteLine("\nDICE ROLLING SIMULATOR RESULTS\n");
+        System.Console.WriteLine("Each '*' represents 1% of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + iNumRolls.ToString() + "\n");
+
+        // For each result, get the asterisks for a histogram
+        System.Console.WriteLine("2: " + histAsterisks(aResults[0], iNumRolls));
+        System.Console.WriteLine("3: " + histAsterisks(aResults[1], iNumRolls));
+        System.Console.WriteLine("4: " + histAsterisks(aResults[2], iNumRolls));
+        System.Console.WriteLine("5: " + histAsterisks(aResults[3], iNumRolls));
+        System.Console.WriteLine("6: " + histAsterisks(aResults[4], iNumRolls));
+        System.Console.WriteLine("7: " + histAsterisks(aResults[5], iNumRolls));
+        System.Console.WriteLine("8: " + histAsterisks(aResults[6], iNumRolls));
+        System.Console.WriteLine("9: " + histAsterisks(aResults[7], iNumRolls));
+        System.Console.WriteLine("10: " + histAsterisks(aResults[8], iNumRolls));
+        System.Console.WriteLine("11: " + histAsterisks(aResults[9], iNumRolls));
+        System.Console.WriteLine("12: " + histAsterisks(aResults[10], iNumRolls));
+
+        System.Console.WriteLine("\nThank you for using the dice rolling simulator. Goodbye!");
+    }
+}
